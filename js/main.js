2941ebf1569a2dcb1f40aab9784999727e432c14
Radovan Markovic <radovanm02@gmail.com>
2026-03-26 23:22:04 +0100

    zavrsen uvod u JavaScript - vezbe02

--- js/main.js
+++ js/main.js
@@ -4,19 +4,25 @@ console.log("Ovo je eksterni JS fajl!");
 naziv = "Dream Lang";
 console.log(naziv);
 
-// Nacini definisanja promenljivih u JS
-
-// var, let, const
-
+// Nacini definisanja promenljivih u JS - var, let, const
 let a = 20;
+a = 30;
+// let a = 40;
 let b = 10;
 console.log("Zbir a i b je: ", a+b);
 
+// hoisting promeljivih
+console.log("Promenljiva grad nije definisana:", grad === undefined);
+var grad  = "Beograd";
+console.log(grad);
+
+// striktne i slabe jednakosti
 let c = "10 ";
 console.log(b==c);
 console.log(b===c);
 
-let temperatura = 12;
+// kontrola toka podataka - if, if else, switch
+let temperatura = 22;
 if (temperatura > 25) {
     console.log("Napolju je toplo");
 } else if (temperatura >15){
@@ -26,9 +32,13 @@ else{
     console.log("Napolju je hladno");
 }
 
-const voce = ['jabuka', 'banana', 'jagoda',1,true];
+// nizovi
+const voce = ['jabuka', 'banana', 'jagoda','kivi'];
 // console.log(voce);
+
+// petlje - for, forEach , for of, for in, while, do while
 for(let i = 0; i<voce.length;i++){
+   
     console.log("Ispis elemenata u nizu:",voce[i]);
 }
 
@@ -38,5 +48,63 @@ for(const v of voce){
 }
 
 for(const vo in voce){
-    console.log(v);
+    // if(vo == 2) break;
+    if(vo ==2) continue;
+    console.log(vo);
 }
+
+
+// Funkcije 
+function PotvrdaForme(){
+    alert("Uspesno ste popunili formu, u najkracem roku potrudicemo se da Vam odgovorimo.");
+}
+
+//funkcija sa argumentima
+function prognoza(temp){
+    if (temp > 25) {
+    console.log("Napolju je toplo");
+} else if (temp >15){
+    console.log("Napolju je prijatno");
+}
+else{
+    console.log("Napolju je hladno");
+}
+}
+prognoza(temperatura);
+
+
+// povratne funkcije
+function saberi(a,b){
+    return a+b;
+}
+console.log(saberi(5,7));
+
+const mnozenje = function(a,b){
+    return a*b;
+}
+
+// Arrow funkcije  () => {}
+const oduzmi = (b1,b2) => {
+    return b1-b2;
+}
+console.log(oduzmi(10,7));
+
+const kvadrat = x => {
+    return x*x;
+}
+console.log(kvadrat(5))
+
+//primer iz skripte
+console.log(voce.map(v => v.length));
+
+console.log("Drugi nacin");
+
+console.log(voce.map(function(element){
+    return element.length;
+}));
+
+
+// IIFE koncept nad anonimnim funkcijama
+(function(name){
+    console.log("Pozdrav iz IIFE sa sajta ", name)
+}(naziv));
